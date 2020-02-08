ed5a78b40a4cdbaf754e79d9f21f6e214b41463c
Dmitriy Litovchenko <msk.roytop@gmail.com>
2020-02-08 20:52:24 +0400

    Added LinkedList structure with Size, GetFirst, GetLast, Clear, RemoveFirst, RemoveLast, InsertLast, GetAt, RemoveAt, InsertAt methods for C#

--- csharp/linked_list/LinkedLintTest.cs
+++ csharp/linked_list/LinkedLintTest.cs
@@ -50,5 +50,156 @@ namespace LinkedList
             list.InsertFirst("b");
             Assert.Equal("b", list.Head.Data);
         }
+
+        [Fact]
+        public void Size()
+        {
+            var list = new LinkedList();
+
+            Assert.Equal(0, list.Size());
+
+            list.InsertFirst("a");
+            list.InsertFirst("b");
+            list.InsertFirst("c");
+            list.InsertFirst("d");
+            list.InsertFirst("e");
+
+            Assert.Equal(5, list.Size());
+        }
+
+        [Fact]
+        public void GetFirst()
+        {
+            var list = new LinkedList();
+            list.InsertFirst("a");
+
+            Assert.Equal("a", list.GetFirst().Data);
+
+            list.InsertFirst("b");
+
+            Assert.Equal("b", list.GetFirst().Data);
+        }
+
+        [Fact]
+        public void GetLast()
+        {
+            var list = new LinkedList();
+            list.InsertFirst("a");
+
+            Assert.Equal("a", list.GetLast().Data);
+
+            list.InsertFirst("b");
+
+            Assert.Equal("a", list.GetLast().Data);
+        }
+
+        [Fact]
+        public void Clear()
+        {
+            var list = new LinkedList();
+            list.InsertFirst("a");
+            list.Clear();
+
+            Assert.Null(list.Head);
+        }
+
+        [Fact]
+        public void RemoveFirst()
+        {
+            var list = new LinkedList();
+            list.InsertFirst("a");
+            list.RemoveFirst();
+
+            Assert.Equal(0, list.Size());
+            Assert.Null(list.Head);
+        }
+
+        [Fact]
+        public void RemoveLast()
+        {
+            var list = new LinkedList();
+            list.InsertFirst("c");
+            list.InsertFirst("b");
+            list.InsertFirst("a");
+            list.RemoveLast();
+
+            Assert.Equal(2, list.Size());
+            Assert.Equal("b", list.GetLast().Data);
+        }
+
+        [Fact]
+        public void InsertLast()
+        {
+            var list = new LinkedList();
+
+            list.InsertFirst("a");
+            list.InsertLast("b");
+
+            Assert.Equal(2, list.Size());
+            Assert.Equal("b", list.GetLast().Data);
+        }
+
+        [Fact]
+        public void GetAt()
+        {
+            var list = new LinkedList();
+
+            Assert.Null(list.GetAt(10));
+
+            list.InsertFirst("a");
+            list.InsertLast("b");
+            list.InsertLast("c");
+            list.InsertLast("d");
+
+            Assert.Equal("a", list.GetAt(0).Data);
+            Assert.Equal("b", list.GetAt(1).Data);
+            Assert.Equal("c", list.GetAt(2).Data);
+            Assert.Equal("d", list.GetAt(3).Data);
+
+        }
+
+        [Fact]
+        public void RemoveAt()
+        {
+            var list = new LinkedList();
+
+            try
+            {
+                list.RemoveAt(0);
+                Assert.True(true);
+            }
+            catch (Exception ex)
+            {
+                Assert.True(false);
+            }
+
+            list.InsertFirst("a");
+            list.InsertLast("b");
+            list.InsertLast("c");
+            list.InsertLast("d");
+
+            Assert.Equal("b", list.GetAt(1).Data);
+            list.RemoveAt(1);
+            Assert.Equal("c", list.GetAt(1).Data);
+
+            list.RemoveAt(2);
+            Assert.Null(list.GetAt(2));
+        }
+
+        [Fact]
+        public void InsertAt()
+        {
+            var list = new LinkedList();
+
+            list.InsertLast("a");
+            list.InsertLast("b");
+            list.InsertLast("c");
+            list.InsertAt("hi", 0);
+
+            Assert.Equal("hi", list.GetAt(0).Data);
+            Assert.Equal("a", list.GetAt(1).Data);
+            Assert.Equal("b", list.GetAt(2).Data);
+            Assert.Equal("c", list.GetAt(3).Data);
+        }
     }
 }
--- csharp/linked_list/LinkedList.cs
+++ csharp/linked_list/LinkedList.cs
@@ -4,6 +4,12 @@ namespace LinkedList
     {
         public Node Head { get; set; }
 
+        /// <summary>
+        /// Creates a new Node from argument 'data'
+        /// and assigns the resulting node to the 'head' property.
+        /// Make sure to handle the case in which the linked list
+        /// already has a node assigned to the 'head' property.
+        /// </summary>
         public void InsertFirst(string data)
         {
             if (this.Head == null) {
@@ -13,6 +19,194 @@ namespace LinkedList
             }
 
             this.Head = new Node(data, this.Head);
+        }
+
+        /// <summary>
+        /// Returns the number of nodes in the linked list.
+        /// </summary>
+        public int Size()
+        {
+            int counter = 0;
+            Node current = this.Head;
+
+            if (current == null)
+            {
+                return counter;
+            }
+
+            while (current != null)
+            {
+                counter++;
+                current = current.Next;
+            }
+
+            return counter;
+        }
+
+        /// <summary>
+        /// Returns the first node of the linked list.
+        /// </summary>
+        public Node GetFirst()
+        {
+            Node result = this.Head;
+
+            return result;
+        }
+
+        /// <summary>
+        /// Returns the last node of the linked list.
+        /// </summary>
+        public Node GetLast()
+        {
+            Node current = this.Head;
+
+            if (current == null)
+            {
+                return current;
+            }
+
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+
+            return current;
+        }
+
+        /// <summary>
+        /// Empties the linked list of any nodes.
+        /// </summary>
+        public void Clear()
+        {
+            this.Head = null;
+        }
+
+        /// <summary>
+        /// Removes only the first node of the linked list.
+        /// The list's head should now be the second element.
+        /// </summary>
+        public void RemoveFirst()
+        {
+            if (this.Head == null)
+            {
+                return;
+            }
+
+            this.Head = this.Head.Next;
+        }
+
+        /// <summary>
+        /// Removes the last node of the chain.
+        /// </summary>
+        public void RemoveLast()
+        {
+            if (this.Head == null)
+            {
+                return;
+            }
+
+            Node current = this.Head;
+            Node next = current.Next;
+
+            while (next.Next != null)
+            {
+                current = next;
+                next = next.Next;
+            }
+
+            current.Next = null;
+        }
+
+        /// <summary>
+        /// Inserts a new node with provided data at the end of the chain
+        /// </summary>
+        public void InsertLast(string data)
+        {
+            var newNode = new Node(data);
+            Node lastNode = this.Head;
+
+            if (lastNode == null)
+            {
+                this.Head = newNode;
+
+                return;
+            }
+
+            while (lastNode.Next != null)
+            {
+                lastNode = lastNode.Next;
+            }
+
+            lastNode.Next = newNode;
+        }
+
+        /// <summary>
+        /// Returns the node at the provided index
+        /// </summary>
+        public Node GetAt(int index)
+        {
+            if (this.Head == null)
+            {
+                return null;
+            }
+
+            Node current = this.Head;
+            int counter = 0;
+
+            while (current != null)
+            {
+                if (index == counter) {
+                    return current;
+                }
+
+                current = current.Next;
+                counter++;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Removes node at the provided index
+        /// </summary>
+        public void RemoveAt(int index)
+        {
+            if (this.Head == null)
+            {
+                return;
+            }
+
+            Node previous = this.GetAt(index - 1);
+            Node next = this.GetAt(index + 1);
+
+            if (previous != null)
+            {
+                previous.Next = next;
+            }
+        }
+
+        /// <summary>
+        /// Removes node at the provided index
+        /// </summary>
+        public void InsertAt(string data, int index)
+        {
+            if (this.Head == null)
+            {
+                return;
+            }
+
+            if (index == 0)
+            {
+                Node temp = this.Head;
+                this.Head = new Node(data, temp);
+
+                return;
+            }
+
+            Node previous = this.GetAt(index - 1);
+            Node current = this.GetAt(index);
+
+            previous.Next = new Node(data, current);
         }
     }
 }

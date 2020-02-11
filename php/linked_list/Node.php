fdfa4dbef104b113b58f29570fd54386207e3b1a
Dmitriy Litovchenko <msk.roytop@gmail.com>
2020-02-11 20:41:04 +0400

    Added LinkedList structure methods: size, getFirst, getLast, clear, removeFirst, removeLast, insertLast, getAt, removeAt, insertAt for PHP

--- php/linked_list/Node.php
+++ php/linked_list/Node.php
@@ -2,12 +2,20 @@
 
 namespace bobel\linked_list;
 
+/**
+ * Creates a class instance to represent a node.
+ * The node should have two properties, 'data' and 'next'.
+ * Accept both of these as arguments to the 'Node' constructor,
+ * then assign them to the instance as properties 'data' and 'next'.
+ * If 'next' is not provided to the constructor,
+ * then default its value to be 'null'.
+ */
 class Node
 {
-    public $date;
+    public $data;
     public ?Node $next;
 
-    public function __construct($data, Node $next = null)
+    public function __construct($data, ?Node $next = null)
     {
         $this->data = $data;
         $this->next = $next;
--- php/linked_list/tests/index.php
+++ php/linked_list/tests/index.php
@@ -24,3 +24,198 @@ test('Insert First appends a node to the start of the list', function () {
     $list->insertFirst(2);
     assertEquals(2, $list->head->data);
 });
+
+test('Size returns the number of items in the linked list', function () {
+    $list = new \bobel\linked_list\LinkedList();
+
+    $list->insertFirst(1);
+    $list->insertFirst(2);
+    $list->insertFirst(3);
+    $list->insertFirst(4);
+    assertEquals(4, $list->size());
+});
+
+test('GetFirst returns the first element', function () {
+    $list = new \bobel\linked_list\LinkedList();
+
+    $list->insertFirst(1);
+    assertEquals(1, $list->GetFirst()->data);
+
+    $list->insertFirst(2);
+    assertEquals(2, $list->GetFirst()->data);
+});
+
+test('GetLast returns the last element', function () {
+    $list = new \bobel\linked_list\LinkedList();
+
+    $list->insertFirst(1);
+    assertEquals(1, $list->GetLast()->data);
+
+    $list->insertFirst(2);
+    assertEquals(1, $list->GetLast()->data);
+});
+
+test('Clear', function () {
+    $list = new \bobel\linked_list\LinkedList();
+
+    $list->insertFirst(1);
+    $list->insertFirst(1);
+    $list->insertFirst(1);
+    $list->insertFirst(1);
+    assertEquals(4, $list->size());
+
+    $list->clear();
+
+    assertEquals(0, $list->size());
+});
+
+test('RemoveFirst removes the first node when the list has a size of one', function () {
+    $list = new \bobel\linked_list\LinkedList();
+
+    $list->insertFirst(1);
+    $list->removeFirst();
+    assertEquals(0, $list->size());
+});
+
+test('RemoveFirst removes the first node when the list has a size of three', function () {
+    $list = new \bobel\linked_list\LinkedList();
+
+    $list->insertFirst(3);
+    $list->insertFirst(2);
+    $list->insertFirst(1);
+    $list->removeFirst();
+    assertEquals(2, $list->size());
+    assertEquals(2, $list->getFirst()->data);
+});
+
+test('RemoveLast removes the last node when list is empty', function () {
+    $list = new \bobel\linked_list\LinkedList();
+
+    try {
+        $list->removeLast();
+        assertTrue(true);
+    } catch (\Exception $e) {
+        assertTrue(false);
+    }
+});
+
+test('RemoveLast removes the last node when list is length 1', function () {
+    $list = new \bobel\linked_list\LinkedList();
+
+    $list->insertFirst(1);
+    $list->removeLast();
+    assertEquals(null, $list->head);
+});
+
+test('RemoveLast removes the last node when list is length 2', function () {
+    $list = new \bobel\linked_list\LinkedList();
+
+    $list->insertFirst(2);
+    $list->insertFirst(1);
+    $list->removeLast();
+
+    assertEquals(1, $list->size());
+    assertEquals(1, $list->getFirst()->data);
+});
+
+test('RemoveLast removes the last node when list is length 3', function () {
+    $list = new \bobel\linked_list\LinkedList();
+
+    $list->insertFirst(3);
+    $list->insertFirst(2);
+    $list->insertFirst(1);
+    $list->removeLast();
+
+    assertEquals(2, $list->size());
+    assertEquals(1, $list->getFirst()->data);
+});
+
+test('InsertLast adds to the end of the list', function () {
+    $list = new \bobel\linked_list\LinkedList();
+
+    $list->insertLast(1);
+    $list->insertLast(2);
+
+    assertEquals(2, $list->getLast()->data);
+
+    $list->insertLast(3);
+
+    assertEquals(1, $list->getFirst()->data);
+    assertEquals(3, $list->getLast()->data);
+});
+
+test('GetAt returns the node at given index', function () {
+    $list = new \bobel\linked_list\LinkedList();
+
+    $list->insertLast(1);
+    $list->insertLast(2);
+    $list->insertLast(3);
+    $list->insertLast(4);
+
+    assertEquals(null, $list->getAt(5));
+
+    assertEquals(1, $list->getAt(0)->data);
+    assertEquals(2, $list->getAt(1)->data);
+    assertEquals(3, $list->getAt(2)->data);
+    assertEquals(4, $list->getAt(3)->data);
+});
+
+test('RemoveAt removeAt doesnt crash on an empty list', function () {
+    $list = new \bobel\linked_list\LinkedList();
+
+    try {
+        $list->removeAt(0);
+        assertTrue(true);
+    } catch (\Exception $e) {
+        assertTrue(false);
+    }
+});
+
+test('RemoveAt doesnt crash on an index out of bounds', function () {
+    $list = new \bobel\linked_list\LinkedList();
+
+    $list->insertLast(1);
+
+    try {
+        $list->removeAt(10);
+        assertTrue(true);
+    } catch (\Exception $e) {
+        assertTrue(false);
+    }
+});
+
+test('RemoveAt works on the last node', function () {
+    $list = new \bobel\linked_list\LinkedList();
+
+    $list->insertLast(1);
+    $list->insertLast(2);
+    $list->insertLast(3);
+    $list->insertLast(4);
+
+    assertEquals(4, $list->getAt(3)->data);
+
+    $list->removeAt(3);
+
+    assertEquals(null, $list->getAt(3));
+});
+
+test('InsertAt inserts a new node with data at the 0 index when the list is empty', function () {
+    $list = new \bobel\linked_list\LinkedList();
+
+    $list->insertAt('hi', 0);
+    assertEquals('hi', $list->getAt(0)->data);
+});
+
+test('InsertAt inserts a new node with data at the 0 index when the list has elements', function () {
+    $list = new \bobel\linked_list\LinkedList();
+
+    $list->insertLast('a');
+    $list->insertLast('b');
+    $list->insertLast('c');
+    $list->insertAt('hi', 0);
+
+    assertEquals('hi', $list->getAt(0)->data);
+    assertEquals('a', $list->getAt(1)->data);
+    assertEquals('b', $list->getAt(2)->data);
+    assertEquals('c', $list->getAt(3)->data);
+});

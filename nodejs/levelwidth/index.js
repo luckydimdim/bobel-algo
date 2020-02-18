22a7819ba03b142282c9f3e1d56543f7b5c5263d
Dmitriy Litovchenko <msk.roytop@gmail.com>
2020-02-18 11:39:33 +0400

    Added tree level width algo for ES6

--- nodejs/levelwidth/index.js
+++ nodejs/levelwidth/index.js
@@ -6,11 +6,34 @@
 // Given:
 //     0
 //   / |  \
-// 1   2   3
-// |       |
-// 4       5
+//  1  2   3
+//  |      |
+//  4      5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    const result = [];
+    const array = [root, 'stop'];
+    let counter = 0;
+
+    while (array.length) {
+        let node = array.shift();
+
+        if (node === 'stop') {
+            if (counter === 0) {
+                return result;
+            }
+
+            result.push(counter);
+            counter = 0;
+            array.push('stop');
+        } else {
+            counter++;
+            array.push(...node.children);
+        }
+    }
+
+    return result;
+}
 
 module.exports = levelWidth;

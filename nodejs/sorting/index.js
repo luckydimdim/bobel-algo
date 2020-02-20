8c75714e58a884fea154900293eb06a9de674da6
Dmitriy Litovchenko <msk.roytop@gmail.com>
2020-02-20 21:21:50 +0400

    Added one more merge sort solution for ECMA6

--- nodejs/sorting/index.js
+++ nodejs/sorting/index.js
@@ -36,9 +36,29 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
+
+  const center = Math.floor(arr.length / 2);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center);
+
+  return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
+  const result = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+
+  return [...result, ...left, ...right];
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };

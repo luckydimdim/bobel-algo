456cf492c665999bd7755da6dd9e5dee34d7362c
Dmitriy Litovchenko <msk.roytop@gmail.com>
2020-02-20 20:50:38 +0400

    Added one more bubble sort solution for ECMA6

--- nodejs/bubble_sort/index.js
+++ nodejs/bubble_sort/index.js
@@ -1,4 +1,4 @@
-function bubbleSort(array) {
+function bubbleSort2(array) {
 	const length = array.length;
 
 	for (let bypass = 1; bypass < length; ++bypass) {
@@ -12,6 +12,20 @@ function bubbleSort(array) {
 	}
 
 	return array;
+}
+
+function bubbleSort(arr) {
+	for (let i = 0; i < arr.length; ++i) {
+		for (let j = 0; j < arr.length - i - 1; ++j) {
+			if (arr[j] > arr[j + 1]) {
+				let temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+
+	return arr;
 }
 
 module.exports = bubbleSort;
